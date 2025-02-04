f03c1d4b20cc8e2b7ef760e459b0b44339522282
me-nata <nataoliveirasousa@gmail.com>
2025-02-03 23:56:08 -0300

    add space

--- src/movement/movement_action.rs
+++ src/movement/movement_action.rs
@@ -5,10 +5,10 @@ pub struct MovementAction(pub Position, pub Position);
 impl MovementAction {
     pub fn move_piece(&self, board: &mut Board) -> MovementStatus {
         let (movement_status, position) = 
-        match  MovementStatus::verify_move_piece(board, self.0, self.1) {
-            Ok((movement, position)) => (movement, position),
-            Err(movement) => return movement
-        };
+            match  MovementStatus::verify_move_piece(board, self.0, self.1) {
+                Ok((movement, position)) => (movement, position),
+                Err(movement) => return movement
+            };
 
         match movement_status {
             MovementStatus::Simple => board.move_piece(self.0, self.1),
