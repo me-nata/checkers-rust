dceb0b8f83efdc2363499ec5c5b91f27e311793b
me-nata <nataoliveirasousa@gmail.com>
2025-02-03 23:47:52 -0300

    example fixing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use common::Position;
 use game::Game;
+use movement::MovementAction;
 
 mod common;
 mod player;
@@ -7,21 +8,16 @@ mod game;
 mod board;
 mod movement;
 
-use movement::MovementStatus;
-
-/// The entry point of the program, initializes a new game and performs a series
-/// of moves, displaying the game state and movement status after each move.
-
 fn main() {
     let mut game = Game::new();
     game.initialize();
     
     let (p1, p2) = (Position(2, 0), Position(3, 0));
-    game.move_piece(p1, p2);
+    game.move_piece(MovementAction(p1, p2));
     
     let (p1, p2) = (Position(5, 1), Position(4, 1));
-    game.move_piece(p1, p2);
+    game.move_piece(MovementAction(p1, p2));
 
     let (p1, p2) = (Position(3, 0), Position(5, 2));
-    game.move_piece(p1, p2);
+    game.move_piece(MovementAction(p1, p2));
 }
